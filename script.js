2b088ccffceb3373eccc1ade1fb2b30bb5d4bd23
shashwat <shashwatshukla1207@gmail.com>
2026-02-24 19:11:15 +0530

    WebD sem 2 initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+document.write("hello world");
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num =false;
+
+const sym1 = Symbol(4)
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else {
+    console.log("false")
+}
+var a=10;var b =13;var c=10; var linebreak ="<br>"
+
+document.write("a+b+c"=);
+result = a+b+c
